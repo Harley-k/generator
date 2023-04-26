3210621bc2d1905c19e5f438519541116bcad891
harley-k <harley_k@s.upc.edu.cn>
2023-04-26 13:52:10 +0800

    feat(111):

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const os = require('os');
 export const formatRouter = (routes, newRoutes = [], parent) => {
-    setTimeout(()=>{
-      document.write('')
-    },3000)
-    routes.forEach((item, index) => {
+    // setTimeout(()=>{
+    //   document.write('')
+    // },3000)
+  console.log(os)
+
+  routes.forEach((item, index) => {
         if (item.children && item.children.length > 0) {
             parent = item.name
                 // routes.push(item)
